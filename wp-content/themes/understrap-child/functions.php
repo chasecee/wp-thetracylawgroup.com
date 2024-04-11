e930e03832c6c011977db18c3256591b4f1af0c0
Chase Christensen <chaserchristensen@gmail.com>
2024-04-11 12:45:56 -0600

    ✨ (functions.php): update code formatting for consistency and readability 📝 (functions.php): add function to enable lazy loading for attachment images to improve performance

--- wp-content/themes/understrap-child/functions.php
+++ wp-content/themes/understrap-child/functions.php
@@ -1,51 +1,66 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
 }
 
-function understrap_remove_scripts() {
-    wp_dequeue_style( 'understrap-styles' );
-    wp_deregister_style( 'understrap-styles' );
+function understrap_remove_scripts()
+{
+    wp_dequeue_style('understrap-styles');
+    wp_deregister_style('understrap-styles');
 
-    wp_dequeue_script( 'understrap-scripts' );
-    wp_deregister_script( 'understrap-scripts' );
+    wp_dequeue_script('understrap-scripts');
+    wp_deregister_script('understrap-scripts');
 
     // Removes the parent themes stylesheet and scripts from inc/enqueue.php
 }
-add_action( 'wp_enqueue_scripts', 'understrap_remove_scripts', 20 );
+add_action('wp_enqueue_scripts', 'understrap_remove_scripts', 20);
 
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-function theme_enqueue_styles() {
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+function theme_enqueue_styles()
+{
 
-	// Get the theme data
-	$the_theme = wp_get_theme();
-    wp_enqueue_style( 'child-understrap-styles', get_stylesheet_directory_uri() . '/css/child-theme.min.css', array(), $the_theme->get( 'Version' ) );
-    wp_enqueue_script( 'jquery');
-    wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $the_theme->get( 'Version' ), true );
-    wp_enqueue_script( 'custom-scripts', get_stylesheet_directory_uri() . '/js/custom.js', array(), $the_theme->get( 'Version' ), true );
-
+    // Get the theme data
+    $the_theme = wp_get_theme();
+    wp_enqueue_style('child-understrap-styles', get_stylesheet_directory_uri() . '/css/child-theme.min.css', array(), $the_theme->get('Version'));
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('child-understrap-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $the_theme->get('Version'), true);
+    wp_enqueue_script('custom-scripts', get_stylesheet_directory_uri() . '/js/custom.js', array(), $the_theme->get('Version'), true);
 }
 
-function add_child_theme_textdomain() {
-    load_child_theme_textdomain( 'understrap-child', get_stylesheet_directory() . '/languages' );
+function add_child_theme_textdomain()
+{
+    load_child_theme_textdomain('understrap-child', get_stylesheet_directory() . '/languages');
 }
-add_action( 'after_setup_theme', 'add_child_theme_textdomain' );
+add_action('after_setup_theme', 'add_child_theme_textdomain');
 
 //// CUSTOM FUNCTIONS by CHASE CEE
 
-require_once( get_stylesheet_directory() . '/custom_functions.php' );
+require_once(get_stylesheet_directory() . '/custom_functions.php');
 
-function wpb_add_google_fonts() {
-wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,700|Playfair+Display:400,700,700i|Carrois+Gothic+SC|Carrois+Gothic', false ); 
+function wpb_add_google_fonts()
+{
+    wp_enqueue_style('wpb-google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,700|Playfair+Display:400,700,700i|Carrois+Gothic+SC|Carrois+Gothic', false);
 }
-add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts' );
+add_action('wp_enqueue_scripts', 'wpb_add_google_fonts');
 
 /** ALLOW CUSTOM MIME TYPES */
 
-function be_enable_vcard_upload( $mime_types ){
+function be_enable_vcard_upload($mime_types)
+{
     $mime_types['vcf'] = 'text/vcard';
     return $mime_types;
-  }
-  add_filter('upload_mimes', 'be_enable_vcard_upload' );
+}
+add_filter('upload_mimes', 'be_enable_vcard_upload');
 
-  add_filter('jpeg_quality', function($arg){return 100;});
+add_filter('jpeg_quality', function ($arg) {
+    return 100;
+});
+
+function add_lazy_loading_to_attachment_images($attr, $attachment, $size)
+{
+    // Add the loading attribute and set it to 'lazy' or 'eager'
+    $attr['loading'] = 'lazy'; // Change 'lazy' to 'eager' if you want the image to load as soon as possible
+    $attr['fetchpriority'] = 'low';
+    return $attr;
+}
+add_filter('wp_get_attachment_image_attributes', 'add_lazy_loading_to_attachment_images', 10, 3);
